f769024f68033e0cc3ca4633dacdd705e1762de7
gyanprakash-github <gyan.prakash@healthassure.in>
2022-12-11 17:21:21 +0530

    Add Development Branch

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,16 +5,19 @@ export default class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;
 
     return (
+      <>
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
         <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+          <a rel="true" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
         </div>
       </div>
       </div>
+      
+      </>
     )
   }
 }
